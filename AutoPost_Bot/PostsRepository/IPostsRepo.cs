b0a1cd05eaafe1ce96d5b474bc29ece1b0d96e23
Dmitriy Germanov <seltis.gtn@gmail.com>
2025-05-11 21:13:55 +0300

    posts loading logic to by bot id changed

--- AutoPost_Bot/PostsRepository/IPostsRepo.cs
+++ AutoPost_Bot/PostsRepository/IPostsRepo.cs
@@ -1,14 +1,14 @@
 ﻿using AutoPost_Bot.Models;
 
-namespace AutoPost_Bot.PostsRepository
+namespace AutoPost_Bot.PostsRepository;
+
+public interface IPostsRepo
 {
-    public interface IPostsRepo
-    {
-        public Task<List<PostModel>> GetPostsAsync();
-        public Task<PostModel?> GetPostByIdAsync(Guid id);
-        public Task<PostModel?> ChangePostByIdAsync(Guid id, PostModel post);
-        public Task<PostModel?> AddPostAsync(PostModel post);
-        public Task SavePostChangesAsync(List<PostModel> postsList);
-        public Task UpdatePostAsync(PostModel post);
-    }
+    public Task<List<PostModel>> GetPostsAsync();
+    public Task<PostModel?> GetPostByIdAsync(Guid id);
+    public Task<PostModel?> ChangePostByIdAsync(Guid id, PostModel post);
+    public Task<PostModel?> AddPostAsync(PostModel post);
+    public Task SavePostChangesAsync(List<PostModel> postsList);
+    public Task UpdatePostAsync(PostModel post);
+    public Task<List<PostModel>> GetPostsByBotTokenAsync(string botToken);
 }
--- AutoPost_Bot/PostsRepository/PostsRepo.cs
+++ AutoPost_Bot/PostsRepository/PostsRepo.cs
@@ -3,75 +3,88 @@ using AutoPost_Bot.Data;
 using AutoPost_Bot.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace AutoPost_Bot.PostsRepository
+namespace AutoPost_Bot.PostsRepository;
+
+public class PostsRepo(PostsContext postsContext, IMapper mapper) : IPostsRepo
 {
-    public class PostsRepo(PostsContext postsContext, IMapper mapper) : IPostsRepo
+    private readonly IMapper _mapper = mapper;
+    private readonly PostsContext _postsContext = postsContext;
+
+    public async Task<PostModel?> AddPostAsync(PostModel post)
     {
-        private readonly PostsContext _postsContext = postsContext;
-        private readonly IMapper _mapper = mapper;
-
-        public async Task<PostModel?> AddPostAsync(PostModel post)
+        try
         {
-            try
-            {
-                _postsContext.Posts.Add(post);
-                await _postsContext.SaveChangesAsync();
-                return post;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return null;
-            }
+            _postsContext.Posts.Add(post);
+            await _postsContext.SaveChangesAsync();
+            return post;
         }
-
-        public Task<PostModel?> ChangePostByIdAsync(Guid id, PostModel post)
+        catch (Exception ex)
         {
-            throw new NotImplementedException();
+            Console.WriteLine(ex.Message);
+            return null;
         }
+    }
 
-        public async Task<PostModel?> GetPostByIdAsync(Guid id)
+    public Task<PostModel?> ChangePostByIdAsync(Guid id, PostModel post)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<PostModel?> GetPostByIdAsync(Guid id)
+    {
+        return await _postsContext.Posts
+            .FirstOrDefaultAsync(post => post.Id == id);
+    }
+
+    public async Task<List<PostModel>> GetPostsAsync()
+    {
+        return await _postsContext.Posts.ToListAsync();
+    }
+
+
+    public async Task SavePostChangesAsync(List<PostModel> postsList)
+    {
+        try
         {
-            return await _postsContext.Posts
-                                      .FirstOrDefaultAsync(post => post.Id == id);
+            _postsContext.ChangeTracker.Clear();
+            var existing = await _postsContext.Posts.ToListAsync();
+            _postsContext.Posts.RemoveRange(existing);
+            await _postsContext.AddRangeAsync(postsList);
+            await _postsContext.SaveChangesAsync();
         }
-
-        public async Task<List<PostModel>> GetPostsAsync() => await _postsContext.Posts.ToListAsync();
-
-
-        public async Task SavePostChangesAsync(List<PostModel> postsList)
+        catch (Exception ex)
         {
-            try
-            {
-                _postsContext.ChangeTracker.Clear();
-                var existing = await _postsContext.Posts.ToListAsync();
-                _postsContext.Posts.RemoveRange(existing);
-                await _postsContext.AddRangeAsync(postsList);
-                await _postsContext.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ошибка при сохранении: " + ex.Message);
-                Console.WriteLine("InnerException: " + ex.InnerException?.Message);
-            }
+            Console.WriteLine("Ошибка при сохранении: " + ex.Message);
+            Console.WriteLine("InnerException: " + ex.InnerException?.Message);
         }
+    }
 
-        public async Task UpdatePostAsync(PostModel post)
+    public async Task UpdatePostAsync(PostModel post)
+    {
+        var dbPost = await _postsContext.Posts.FindAsync(post.Id);
+
+        if (dbPost != null) _mapper.Map(dbPost, post);
+        try
         {
-            var dbPost = await _postsContext.Posts.FindAsync(post.Id);
+            await _postsContext.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 
-            if (dbPost != null)
-            {
-                _mapper.Map(dbPost, post);
-            }
-            try
-            {
-                await _postsContext.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+    public async Task<List<PostModel>> GetPostsByBotTokenAsync(string botToken)
+    {
+        try
+        {
+            return await _postsContext.Posts.Where(post => post.BotID != null && post.BotID.Equals(botToken))
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
         }
     }
 }
