f5cc07ea0d285c9f56fbcbce8d34e712cbb87793
Dmitriy Germanov <seltis.gtn@gmail.com>
2025-05-22 22:04:00 +0300

    PasswordGenerate and verify added

--- AutoPost_Bot/Users/IUsersRepo.cs
+++ AutoPost_Bot/Users/IUsersRepo.cs
@@ -5,11 +5,11 @@ namespace AutoPost_Bot.UsersRepo;
 public interface IUsersRepo
 {
     public Task<UserModel> CreateUserAsync(UserModel user);
-    public Task<UserModel> CreateUserAsync(long userId, string password);
+    public Task<UserModel> CreateUserAsync(string email, string password);
     public Task<UserModel> UpdateUserAsync(UserModel user);
     public Task<UserModel> DeleteUserAsync(UserModel user);
     public Task<UserModel> DeleteUserAsync(long userId);
-    public Task<UserModel> FindUserAsync(long userId);
+    public Task<UserModel?> FindUserAsync(string email);
     public Task<bool> LoginUserAsync(UserModel user);
     public Task<bool> LoginUserAsync(long userId, string password);
 }
--- AutoPost_Bot/Users/UsersRepo.cs
+++ AutoPost_Bot/Users/UsersRepo.cs
@@ -1,18 +1,52 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using AutoPost_Bot.Data;
 using AutoPost_Bot.Models;
+using AutoPost_Bot.UsersRepo;
 
-namespace AutoPost_Bot.UsersRepo;
+namespace AutoPost_Bot.Users;
 
-public partial class UsersRepo : IUsersRepo
+public partial class UsersRepo(IServiceProvider serviceProvider) : IUsersRepo
 {
+    private readonly UserContext? _userContext = serviceProvider.GetService<UserContext>();
+
     public Task<UserModel> CreateUserAsync(UserModel user)
     {
         throw new NotImplementedException();
     }
 
-    public Task<UserModel> CreateUserAsync(long userId, string password)
+    public async Task<UserModel> CreateUserAsync(string email, string password)
     {
-        throw new NotImplementedException();
+        if (await FindUserAsync(email).ConfigureAwait(false) is not null)
+            throw new Exception("User already exists");
+
+        var hash = GeneratePasswordHash(password, out byte[] salt);
+
+        var newUser = new UserModel
+        {
+            Email = email,
+            PasswordHash = hash,
+            PasswordSalt = salt,
+            RoleId = RoleId.User
+        };
+
+        try
+        {
+            if (_userContext == null)
+                throw new Exception("UserContext can't be null.");
+
+            _userContext.Users.Add(newUser);
+            await _userContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+
+        return newUser;
     }
 
     public Task<UserModel> UpdateUserAsync(UserModel user)
@@ -30,7 +64,7 @@ public partial class UsersRepo : IUsersRepo
         throw new NotImplementedException();
     }
 
-    public Task<UserModel> FindUserAsync(long userId)
+    public async Task<UserModel?> FindUserAsync(string email)
     {
         throw new NotImplementedException();
     }
@@ -47,4 +81,21 @@ public partial class UsersRepo : IUsersRepo
 
     [GeneratedRegex(@"^[\w.+-]+@[\w.-]+\.[a-zA-Z]{2,}$")]
     private static partial Regex EmailRegex();
+
+    private byte[] GeneratePasswordHash(string password, out byte[] salt)
+    {
+        salt = new byte[16];
+        new Random().NextBytes(salt);
+
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+        return SHA256.HashData(passwordBytes.Concat(salt).ToArray());
+    }
+
+    private bool VerifyPasswordHash(string password, byte[] salt, byte[] passwordHash)
+    {
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+        return Equals(SHA256.HashData(passwordBytes.Concat(salt).ToArray()), passwordHash);
+    }
 }
