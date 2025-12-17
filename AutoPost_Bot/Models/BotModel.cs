0a32689d97f363f6c68a21429de3139e415b4d1f
Dmitriy Germanov <seltis.gtn@gmail.com>
2025-12-17 22:55:30 +0300

    added the ability to give bots names

--- AutoPost_Bot/Models/BotModel.cs
+++ AutoPost_Bot/Models/BotModel.cs
@@ -5,6 +5,7 @@
         public Guid BotId { get; set; }
         public string Token { get; set; } = string.Empty;
         public bool IsActive { get; set; }
+        public string? BotName { get; set; }
 
         public virtual IEnumerable<GroupModel>? Groups { get; set; }
         public virtual List<PostModel>? Posts { get; set; }
